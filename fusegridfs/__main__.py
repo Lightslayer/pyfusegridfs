85fb62a1c2104e9d0d56d46e1ac303b9d4cfc924
Liam Deacon <liam.deacon@nats.co.uk>
2018-02-05 12:51:04 +0000

    Added main() function

--- fusegridfs/__main__.py
+++ fusegridfs/__main__.py
@@ -1,21 +1,26 @@
 import llfuse
+import sys
 
 from .argv import parser
 from .fuse import GridFSOperations
 from .log import setup_logging
 
+def main(argv=sys.argv):
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    setup_logging(debug=args.debug)
+    operations = GridFSOperations(args.host, args.db, args.collection)
 
-setup_logging(debug=args.debug)
-operations = GridFSOperations(args.host, args.db, args.collection)
+    llfuse.init(operations, args.mountpoint, ['fsname=gridfs'])
 
-llfuse.init(operations, args.mountpoint, ['fsname=gridfs'])
+    try:
+        llfuse.main(single=args.single)
+    except:
+        llfuse.close()
+        raise
+    finally:
+        llfuse.close()
 
-try:
-    llfuse.main(single=args.single)
-except:
-    llfuse.close()
-    raise
-finally:
-    llfuse.close()
+if __name__ == '__main__':
+    main()
+

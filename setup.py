9edb61c4b515a4a989859953b175980b5239a42d
Liam Deacon <liam.deacon@nats.co.uk>
2018-02-05 12:50:32 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="pyfusegridfs",
+    description='Mount MongoDB GridFS as FUSE filesystem using Python',
+    author='Serge Matveenko',
+    version=os.open('VERSION').read() or '0.0.1',
+    install_requires=os.open('requirements.txt').readlines(),
+    include_package_data=True,
+    package_dir={'pyfusegridfs': 'fusegridfs'},
+    packages=find_packages('.'),
+    entry_points={
+        'console_scripts': [
+            'pyfusegridfs = fusegridfs.__main__:main',
+        ]
+    }
+)
